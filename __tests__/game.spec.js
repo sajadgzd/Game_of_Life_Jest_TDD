ede2478d86c95aa0792452941e3848080ac47f67
sajad1993gh <sajad1993gh@gmail.com>
2020-01-24 11:55:26 -0500

    tests written

--- __tests__/game.spec.js
+++ __tests__/game.spec.js
@@ -1,16 +1,21 @@
 require("../game");
-const game = window.game;
+const { isAlive } = window.game;
 
 
 
-describe("our game module", ()=> {
-    test("game", () => {
-        expect(game).toBeDefined();
-    })
-    test("should be defined", () => {
-        const name = "Sajad"
-        expect(game.hello(name)).toBeDefined();
-        expect(game.hello(name)).toEqual("Hello Sajad");
-        
+describe("game of life", ()=> {
+    describe("isAlive algorithm", () => {
+        test("dead cell with 0 neighbours return 0", () => {
+            expect(isAlive(0, 0)).toEqual(0);
+        })
+        test("dead cell with 3 neighbours return 1", () => {
+            expect(isAlive(0, 3)).toEqual(1);
+        })
+        test("live cell with 0 neighbours return 0", () => {
+            expect(isAlive(1, 0)).toEqual(0);
+        })
+        test("live cell with 2 neighbours return 1", () => {
+            expect(isAlive(1, 2)).toEqual(1);
+        })
     })
 })
--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+const isAlive = (cell, neighbours) =>
+  (neighbours === 3 || cell && neighbours == 2)? 1 : 0
+
+
 window.game = {
-  hello: name => `Hello ${name}`
+  isAlive
 };
