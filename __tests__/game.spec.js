6a76052cda9507140cd19e992542f9073fcd0ca2
sajad1993gh <sajad1993gh@gmail.com>
2020-01-24 12:09:44 -0500

    developeed tests

--- __tests__/game.spec.js
+++ __tests__/game.spec.js
@@ -1,5 +1,5 @@
 require("../game");
-const { isAlive } = window.game;
+const { isAlive, generate, regenerate } = window.game;
 
 
 
@@ -16,6 +16,27 @@ describe("game of life", ()=> {
         })
         test("live cell with 2 neighbours return 1", () => {
             expect(isAlive(1, 2)).toEqual(1);
+        })
+    })
+
+    describe("generate function", () => {
+        test("should create an array of x * x", () => {
+            expect(generate(1)).toEqual([0])
+            expect(generate(2)).toEqual([0, 0, 0, 0])
+        })
+    })
+
+    describe("regenerate function", () => {
+        test("should not update dead cells", () => {
+            const cells = generate(1)
+            expect(regenerate(cells)).toEqual(cells)
+        })
+
+        test("should update", () => {
+            const initialCells = generate(2)
+            const cells = generate(2)
+            cells[0] = 1
+            expect(regenerate(cells)).toEqual(initialCells)
         })
     })
 })
--- game.js
+++ game.js
@@ -1,7 +1,12 @@
 const isAlive = (cell, neighbours) =>
   (neighbours === 3 || cell && neighbours == 2)? 1 : 0
 
+const generate = (root) => new Array(root * root).fill(0)
+
+const regenerate = cells => cells.map(cell => isAlive(cell, 0))
 
 window.game = {
-  isAlive
+  isAlive,
+  generate,
+  regenerate
 };
